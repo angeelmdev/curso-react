79f8f572f539a68dd9ec24324c913a74610321a9
angeelmdev <anmorenomunoz@gmail.com>
2025-06-22 22:25:41 +0200

    feat: add new project shopping-cart

---
+++ shopping-cart/src/main.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client'
+import { FiltersProvider } from './context/filters.jsx'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <FiltersProvider>
+    <App />
+  </FiltersProvider>
+)
